60eed6fc648bf056e39ac6001f0aeafc55bd97f3
gabriel.tudorica@ymail.com <gabriel.tudorica@ymail.com>
2014-12-04 00:19:04 +0200

    Add SymbolTypeConverter

---
+++ Tic Tac Toe/TicTacToe.Game/SymbolTypeConverter.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Configuration;
+
+namespace TicTacToe.Game
+{
+    public class SymbolTypeConverter
+    {
+        private readonly Dictionary<SymbolTypes, string> stringSymbols;
+
+        public SymbolTypeConverter()
+        {
+            stringSymbols = new Dictionary<SymbolTypes, string>
+                                {
+                                    {SymbolTypes.Cross, ConfigurationManager.AppSettings["NoughtSymbol"]},
+                                    {SymbolTypes.Nought, ConfigurationManager.AppSettings["CrossSymbol"]},
+                                    {SymbolTypes.Uninitialized, string.Empty}
+                                };
+        }
+
+        public string GetString(SymbolTypes symbol)
+        {
+            return stringSymbols[symbol];
+        }
+    }
+}
