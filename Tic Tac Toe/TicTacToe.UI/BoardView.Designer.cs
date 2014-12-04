dc53175b1e62604cfe72e563f188bb13570a53a4
gabriel.tudorica@ymail.com <gabriel.tudorica@ymail.com>
2014-12-04 23:45:56 +0200

    Updated UI colors/positions for better readibility

--- Tic Tac Toe/TicTacToe.UI/BoardView.Designer.cs
+++ Tic Tac Toe/TicTacToe.UI/BoardView.Designer.cs
@@ -36,8 +36,6 @@
             this.noughtsWinsCount = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.drawsCount = new System.Windows.Forms.Label();
-            this.label7 = new System.Windows.Forms.Label();
-            this.label8 = new System.Windows.Forms.Label();
             this.label9 = new System.Windows.Forms.Label();
             this.nextTurn = new System.Windows.Forms.Label();
             this.SuspendLayout();
@@ -49,45 +47,45 @@
             this.board.Cursor = System.Windows.Forms.Cursors.Arrow;
             this.board.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.board.ForeColor = System.Drawing.Color.Yellow;
-            this.board.Location = new System.Drawing.Point(75, 74);
+            this.board.Location = new System.Drawing.Point(60, 130);
+            this.board.Margin = new System.Windows.Forms.Padding(4);
             this.board.Name = "board";
-            this.board.Size = new System.Drawing.Size(375, 325);
+            this.board.Size = new System.Drawing.Size(400, 400);
             this.board.TabIndex = 0;
-            this.board.Width = 300;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.Color.Gray;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(42, 9);
+            this.label1.Location = new System.Drawing.Point(65, 11);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(78, 20);
+            this.label1.Size = new System.Drawing.Size(89, 25);
             this.label1.TabIndex = 1;
-            this.label1.Text = "\'X\' Wins:";
+            this.label1.Text = "X Wins:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.BackColor = System.Drawing.Color.Gray;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.ForeColor = System.Drawing.Color.White;
-            this.label2.Location = new System.Drawing.Point(42, 51);
+            this.label2.Location = new System.Drawing.Point(63, 44);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(79, 20);
+            this.label2.Size = new System.Drawing.Size(91, 25);
             this.label2.TabIndex = 2;
-            this.label2.Text = "\'O\' Wins:";
+            this.label2.Text = "O Wins:";
             // 
             // crossesWinsCount
             // 
             this.crossesWinsCount.AutoSize = true;
             this.crossesWinsCount.BackColor = System.Drawing.Color.Gray;
             this.crossesWinsCount.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.crossesWinsCount.ForeColor = System.Drawing.Color.White;
-            this.crossesWinsCount.Location = new System.Drawing.Point(141, 9);
+            this.crossesWinsCount.Location = new System.Drawing.Point(171, 11);
+            this.crossesWinsCount.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.crossesWinsCount.Name = "crossesWinsCount";
-            this.crossesWinsCount.Size = new System.Drawing.Size(0, 20);
+            this.crossesWinsCount.Size = new System.Drawing.Size(0, 25);
             this.crossesWinsCount.TabIndex = 3;
             // 
             // noughtsWinsCount
@@ -95,10 +93,10 @@
             this.noughtsWinsCount.AutoSize = true;
             this.noughtsWinsCount.BackColor = System.Drawing.Color.Gray;
             this.noughtsWinsCount.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.noughtsWinsCount.ForeColor = System.Drawing.Color.White;
-            this.noughtsWinsCount.Location = new System.Drawing.Point(141, 51);
+            this.noughtsWinsCount.Location = new System.Drawing.Point(171, 44);
+            this.noughtsWinsCount.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.noughtsWinsCount.Name = "noughtsWinsCount";
-            this.noughtsWinsCount.Size = new System.Drawing.Size(0, 20);
+            this.noughtsWinsCount.Size = new System.Drawing.Size(0, 25);
             this.noughtsWinsCount.TabIndex = 4;
             // 
             // label5
@@ -106,10 +104,10 @@
             this.label5.AutoSize = true;
             this.label5.BackColor = System.Drawing.Color.Gray;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.ForeColor = System.Drawing.Color.White;
-            this.label5.Location = new System.Drawing.Point(201, 32);
+            this.label5.Location = new System.Drawing.Point(75, 76);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(64, 20);
+            this.label5.Size = new System.Drawing.Size(79, 25);
             this.label5.TabIndex = 5;
             this.label5.Text = "Draws:";
             // 
@@ -118,45 +116,21 @@
             this.drawsCount.AutoSize = true;
             this.drawsCount.BackColor = System.Drawing.Color.Gray;
             this.drawsCount.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.drawsCount.ForeColor = System.Drawing.Color.White;
-            this.drawsCount.Location = new System.Drawing.Point(283, 32);
+            this.drawsCount.Location = new System.Drawing.Point(171, 76);
+            this.drawsCount.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.drawsCount.Name = "drawsCount";
-            this.drawsCount.Size = new System.Drawing.Size(0, 20);
+            this.drawsCount.Size = new System.Drawing.Size(0, 25);
             this.drawsCount.TabIndex = 6;
-            // 
-            // label7
-            // 
-            this.label7.AutoSize = true;
-            this.label7.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            this.label7.Font = new System.Drawing.Font("Segoe Script", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.ForeColor = System.Drawing.Color.White;
-            this.label7.Location = new System.Drawing.Point(41, 428);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(159, 30);
-            this.label7.TabIndex = 5;
-            this.label7.Text = "Designed By:  ";
-            // 
-            // label8
-            // 
-            this.label8.AutoSize = true;
-            this.label8.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            this.label8.Font = new System.Drawing.Font("Segoe Script", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label8.ForeColor = System.Drawing.Color.White;
-            this.label8.Location = new System.Drawing.Point(200, 428);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(222, 30);
-            this.label8.TabIndex = 8;
-            this.label8.Text = "Ehtesham Mehmood";
             // 
             // label9
             // 
             this.label9.AutoSize = true;
             this.label9.BackColor = System.Drawing.Color.Gray;
             this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label9.ForeColor = System.Drawing.Color.White;
-            this.label9.Location = new System.Drawing.Point(347, 32);
+            this.label9.Location = new System.Drawing.Point(258, 45);
+            this.label9.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(50, 20);
+            this.label9.Size = new System.Drawing.Size(64, 25);
             this.label9.TabIndex = 9;
             this.label9.Text = "Turn:";
             // 
@@ -165,25 +139,23 @@
             this.nextTurn.AutoSize = true;
             this.nextTurn.BackColor = System.Drawing.Color.Gray;
             this.nextTurn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.nextTurn.ForeColor = System.Drawing.Color.White;
-            this.nextTurn.Location = new System.Drawing.Point(413, 32);
+            this.nextTurn.Location = new System.Drawing.Point(346, 45);
+            this.nextTurn.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.nextTurn.Name = "nextTurn";
-            this.nextTurn.Size = new System.Drawing.Size(0, 20);
+            this.nextTurn.Size = new System.Drawing.Size(0, 25);
             this.nextTurn.TabIndex = 9;
             // 
             // BoardView
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlLight;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
-            this.ClientSize = new System.Drawing.Size(477, 467);
-            this.Controls.Add(this.nextTurn);
-            this.Controls.Add(this.label9);
-            this.Controls.Add(this.label8);
-            this.Controls.Add(this.label7);
+            this.ClientSize = new System.Drawing.Size(447, 499);
             this.Controls.Add(this.drawsCount);
             this.Controls.Add(this.label5);
+            this.Controls.Add(this.nextTurn);
+            this.Controls.Add(this.label9);
             this.Controls.Add(this.noughtsWinsCount);
             this.Controls.Add(this.crossesWinsCount);
             this.Controls.Add(this.label2);
@@ -191,10 +163,11 @@
             this.Controls.Add(this.board);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.MaximizeBox = false;
             this.Name = "BoardView";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "TIC TAC TOE";
+            this.Text = "Tic Tac Toe";
             this.TransparencyKey = System.Drawing.Color.Transparent;
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -210,8 +183,6 @@
         private System.Windows.Forms.Label noughtsWinsCount;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label drawsCount;
-        private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.Label label8;
         private System.Windows.Forms.Label label9;
         private System.Windows.Forms.Label nextTurn;
     }
